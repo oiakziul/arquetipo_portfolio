00bb37b817059152c6ef13e3cfd59f122ec97ea9
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-04-03 12:58:51 -0300

    Primeira mensagem Linuxx

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,32 +1,43 @@
 
 
 // interface Props {
-  
+
 // }
 
 export const Inicio = () => {
   return (
-    
-    <div style={{marginLeft:'20px'}}>
+
+    <div style={{ marginLeft: '20px' }}>
       <br /><br />
-      <h1 style={{fontSize:'30px'}}>Notas da versão:</h1> <br /> <br />
+      <h1 style={{ fontSize: '30px' }}>Notas da versão:</h1> <br /> <br />
       <h1 >
-       Hoje 25/03/2025 <br /> <br />
-       1 - modularização com styled componentes concluida com sucesso. ✔✔
+        Hoje 03/04/2025 <br /> <br />
+        <br /> <br /> 01 - Modularização com styled componentes concluida com sucesso. ✔✔
+        <br /> <br /> 02 - Componentização com arquivos .tsx implementado com sucesso. ✔✔
+        <br /> <br /> 03 - Router DOM & SPA implementado com sucesso. ✔✔
+        <br /> <br /> 04 - Repositório migrado para o Linux com sucesso. ✔✔
+        <br /> <br /> 05 - Logica do GRID integrada ao Router DOM concluida com suecsso. ✔✔
       </h1> <br /> <br />
-      <h1>
-       2 - Proximo passo, recuperar a responsividade que foi retirada do código para manutenção. 
-      </h1>
-      <br /> <br />
 
       <h1>
-       3-  Criar a tela inicial definitiva.
-      </h1>
-      <br /> <br />
-      
+        06 - Próximos passos,
+        Recuperar a responsividade que foi retirada do código para manutenção
+        <br /><br /> (Troquei o nome de todas as classes para facilitar a manutenção e evoluição futuramente).
+      </h1> <br /> <br />
+
+      <h1>
+        07 -  Criar a tela inicial definitiva.
+      </h1>      <br /> <br />
+
       <h2>
-          4 - Criar a tela 'Sobre mim'.
-      </h2>
+        08 - Criar a tela 'Sobre mim'.
+      </h2> <br /> <br />
+
+      <h2>
+        09 - Tailwind ou MUI?</h2>
+      <h2> <br /> <br />
+        10 - Segredos ...
+      </h2><br /> <br />
 
     </div>
   );
