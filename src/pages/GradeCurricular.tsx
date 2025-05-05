74033f7c43f4dfdfa3358ff9a9fbea19a7477786
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-05 16:01:12 -0300

    Alterando a grade

--- src/pages/GradeCurricular.tsx
+++ src/pages/GradeCurricular.tsx
@@ -1,40 +1,288 @@
 import styles from '../assets/styles/components/gradeCurricular.module.css';
 
-// interface Props {
-  
-// }
-
 export const GradeCurricular = () => {
   return (
-    
     <div className={styles.container}>
-      
       <table className={styles.table}>
         <thead>
           <tr>
+            <th>Ordem</th>
             <th>Disciplina</th>
-            <th>Ano</th>
+            <th>Ano/Módulo</th>
             <th>Situação</th>
           </tr>
         </thead>
         <tbody>
+        <tr>
+            <td>0</td>
+            <td>GO - PROJETO DE VIDA</td>
+            <td>2022/51</td>
+            <td>Concluído</td>
+          </tr>
           <tr>
+            <td>1ª</td>
+            <td>ADMINISTRAÇÃO DE CONFLITOS E RELACIONAMENTOS</td>
+            <td>2022/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>2ª</td>
             <td>FUNDAMENTOS E ARQUITETURA DE COMPUTADORES</td>
-            <td>2022</td>
+            <td>2022/51</td>
             <td>Concluído</td>
-            
           </tr>
           <tr>
+            <td>3ª</td>
             <td>ALGORITMOS E LÓGICA DE PROGRAMAÇÃO I</td>
-            <td>2022</td>
+            <td>2022/52</td>
             <td>Concluído</td>
           </tr>
           <tr>
-            <td>a fazer</td>
-            <td>+31</td>
-            <td>+31</td>
+            <td>4ª</td>
+            <td>ALGORITMOS E LÓGICA DE PROGRAMAÇÃO II</td>
+            <td>2022/52</td>
+            <td>Concluído</td>
           </tr>
-          {/* Adicione mais linhas <tr> aqui para preencher a tabela */}
+          <tr>
+            <td>5ª</td>
+            <td>ENGENHARIA DE SOFTWARE</td>
+            <td>2022/53</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>6ª</td>
+            <td>ESTATÍSTICA</td>
+            <td>2022/53</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>7ª</td>
+            <td>ANÁLISE E PROJETO ORIENTADO A OBJETOS</td>
+            <td>2022/54</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>8ª</td>
+            <td>ENGENHARIA DE REQUISITOS</td>
+            <td>2022/54</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>9ª</td>
+            <td>LÓGICA PARA COMPUTAÇÃO</td>
+            <td>2023/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>10ª</td>
+            <td>MODELAGEM DE SOFTWARE</td>
+            <td>2023/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>11ª</td>
+            <td>ESTRUTURA DE DADOS I</td>
+            <td>2023/52</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>12ª</td>
+            <td>ESTRUTURA DE DADOS II</td>
+            <td>2023/52</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>13ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: INOVAÇÃO E PENSAMENTO CRIATIVO</td>
+            <td>2023/53</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>14ª</td>
+            <td>PARADIGMAS DE LINGUAGEM DE PROGRAMAÇÃO</td>
+            <td>2023/53</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>15ª</td>
+            <td>PROJETO, IMPLEMENTAÇÃO E TESTE DE SOFTWARE</td>
+            <td>2023/53</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>16ª</td>
+            <td>CÁLCULO DIFERENCIAL E INTEGRAL</td>
+            <td>2023/54</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>17ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: INDÚSTRIA E TRANSFORMAÇÃO DIGITAL</td>
+            <td>2023/54</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>18ª</td>
+            <td>SISTEMAS OPERACIONAIS</td>
+            <td>2023/54</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>19ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: LEITURA DE IMAGENS, GRÁFICOS E MAPAS</td>
+            <td>2024/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>20ª</td>
+            <td>FORMAÇÃO SOCIOCULTURAL E ÉTICA I</td>
+            <td>2024/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>21ª</td>
+            <td>PESQUISA OPERACIONAL</td>
+            <td>2024/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>22ª</td>
+            <td>QUALIDADE DE SOFTWARE</td>
+            <td>2024/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>23ª</td>
+            <td>BANCO DE DADOS I</td>
+            <td>2024/52</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>24ª</td>
+            <td>BANCO DE DADOS II</td>
+            <td>2024/52</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>25ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: PROPRIEDADE INTELECTUAL</td>
+            <td>2024/52</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>26ª</td>
+            <td>FORMAÇÃO SOCIOCULTURAL E ÉTICA II</td>
+            <td>2024/52</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>27ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: INTELIGÊNCIA EMOCIONAL</td>
+            <td>2024/53</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>28ª</td>
+            <td>GERENCIAMENTO DE SOFTWARE</td>
+            <td>2024/53</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>29ª</td>
+            <td>TÓPICOS EM COMPUTAÇÃO I</td>
+            <td>2024/53</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>30ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: ADMINISTRAÇÃO DE CONFLITO</td>
+            <td>2024/54</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>31ª</td>
+            <td>PROGRAMAÇÃO DE SISTEMAS I</td>
+            <td>2024/54</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>32ª</td>
+            <td>PROGRAMAÇÃO DE SISTEMAS II</td>
+            <td>2024/54</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>33ª</td>
+            <td>PESTUDO CONTEMPORÂNEO E TRANSVERSAL: GESTÃO DO CONHECIMENTO</td>
+            <td>2025/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>34ª</td>
+            <td>TÓPICOS ESPECIAIS EM ENGENHARIA DE SOFTWARE I - FABRICA DE SOFTWARE</td>
+            <td>2025/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>35ª</td>
+            <td>DESIGN E INTERAÇÃO</td>
+            <td>2025/51</td>
+            <td>Concluído</td>
+          </tr>
+          <tr>
+            <td>36ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: COMUNICAÇÃO ASSERTIVA E INTERPESSOAL</td>
+            <td>2025/52</td>
+            <td>Cursando</td>
+          </tr>
+          <tr>
+            <td>37ª</td>
+            <td>TÓPICOS EM COMPUTAÇÃO II</td>
+            <td>2025/52</td>
+            <td>Cursando</td>
+          </tr>
+          <tr>
+            <td>38ª</td>
+            <td>EMPREENDEDORISMO</td>
+            <td>2025/52</td>
+            <td>Cursando</td>
+          </tr>
+          <tr>
+            <td>39ª</td>
+            <td>FUNDAMENTOS DE REDES DE COMPUTADORES</td>
+            <td>2025/53</td>
+            <td>Matriculado</td>
+          </tr>
+          <tr>
+            <td>40ª</td>
+            <td>GESTÃO DE PROJETOS</td>
+            <td>2025/53</td>
+            <td>Matriculado</td>
+          </tr>
+          <tr>
+            <td>41ª</td>
+            <td>SEGURANÇA E AUDITORIA DE SISTEMAS</td>
+            <td>2025/53</td>
+            <td>Matriculado</td>
+          </tr>
+          <tr>
+            <td>42ª</td>
+            <td>TÓPICOS ESPECIAIS EM ENGENHARIA DE SOFTWARE II</td>
+            <td>2025/54</td>
+            <td>Matriculado</td>
+          </tr>
+          <tr>
+            <td>43ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: Tema ainda não divulgado</td>
+            <td>2025/54</td>
+            <td>Matriculado</td>
+          </tr>
+          <tr>
+            <td>44ª</td>
+            <td>ESTUDO CONTEMPORÂNEO E TRANSVERSAL: Tema ainda não divulgado</td>
+            <td>2025/54</td>
+            <td>Matriculado</td>
+          </tr>
         </tbody>
       </table>
     </div>
