d16a5830cab0173117fa792b8774c56104fe51a5
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-06 20:01:13 -0300

    css2.

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -9,16 +9,14 @@ export const Inicio = () => {
         Hoje 06/05/2025 <br /> <br />
         <br /> <br /> v1.1 - Modularização - Styled Componentes. <span className={styles.check}>✔✔</span>
         <br /> <br /> v1.2 - Componentização .tsx implementada. <span className={styles.check}>✔✔</span>
-        <br /> <br /> v1.3 - Router DOM & SPA implementado com sucesso. <span className={styles.check}>✔✔</span>
+        <br /> <br /> v1.3 - Router DOM & SPA implementado. <span className={styles.check}>✔✔</span>
         <br /> <br /> v1.4 - Repositório migrado para o Linux. <span className={styles.check}>✔✔</span>
         <br /> <br /> v1.5 - Lógica do GRID integrada ao Router DOM. <span className={styles.check}>✔✔</span>
-        <br /> <br /> v1.6 - Responsividade: Layout otimizado em telas menores. <span className={styles.check}>✔✔</span>
+        <br /> <br /> v1.6 - Responsividade otimizada para telas menores. <span className={styles.check}>✔✔</span>
         <br /> <br /> v1.7 - Componente 'Grade', Finalizada. <span className={styles.check}>✔✔</span> 
         <br /> <br /> v1.8 - Implementação de uso offline conlcuida. <span className={styles.check}>✔✔</span>
         <br /> <br /> v1.9 - <span className={styles.corAviso}> . . . </span>  
       </h1> <br /> <br />
-
-      
     </div>
   );
 };
