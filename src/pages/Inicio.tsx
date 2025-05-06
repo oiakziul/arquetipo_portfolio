0eda136115b7e0aa9b56b7c0792f7c57a4f2962f
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-06 19:43:01 -0300

    footer corrigido.

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -12,33 +12,12 @@ export const Inicio = () => {
         <br /> <br /> 04 - Repositório migrado para o Linux com sucesso. ✔✔
         <br /> <br /> 05 - Logica do GRID integrada ao Router DOM concluída com sucesso. ✔✔
         <br /> <br /> 06 - Responsividade aplicada, layout alterado para telas menores. ✔✔
-        <br /> <br /> 06 - Tela 'Grade', Finalizada. ✔✔
-        <br /> <br /> 07 - Implementação de uso offline conlcuida. ✔✔
+        <br /> <br /> 07 - Tela 'Grade', Finalizada. ✔✔
+        <br /> <br /> 08 - Implementação de uso offline conlcuida. ✔✔
       </h1> <br /> <br />
 
-      <h1>
-        08 -  Criar a tela inicial definitiva.
-      </h1>      <br /> <br />
-
-      <h2>
-        09 - Criar a tela 'Sobre mim'.
-      </h2> <br /> <br />
-
-      <h2>
-        10 - Tailwind ou MUI?</h2>
-      <h2> <br /> <br />
-        11 - Incremento de um segundo tipo de curriculo com estrutura .tsx
-      </h2><br /> <br />
-
       <h2> 
-        12 - Botões de download dos pdfs dos arquivos disponiveis.
-      </h2><br /> <br />
-
-      <h2> 
-        13 - Terminar de popular a tela 'Faculdade'...
-      </h2><br /> <br />
-      <h2> 
-        14 - ...
+        09 - ...
       </h2><br /> <br />
     </div>
   );
