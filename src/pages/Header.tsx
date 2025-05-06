73c1a5b8f982ac72f67d16af11154d75e1a74ed9
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-05 22:17:55 -0300

    response21

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,8 +1,13 @@
-import styles from '../assets/styles/components/header.module.css'
+import { MdOutlineInstallDesktop, MdOutlineInstallMobile } from 'react-icons/md';
+import styles from '../assets/styles/components/header.module.css'; // Seu import de CSS module
+
 export const Header = () => {
     return (
         <header className={styles.header}>
             <h3>Portfólio - SPA</h3>
+            <button className={styles.buttonPWA }>
+                Use Offline <MdOutlineInstallMobile /> <MdOutlineInstallDesktop />
+            </button>
         </header>
-    )
-}
+    );
+};
