def60c6d0d249ad30c770df49a87aa45fe055205
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-07 17:19:29 -0300

    instalavel3

--- src/pages/Pwa.tsx
+++ src/pages/Pwa.tsx
@@ -9,7 +9,7 @@ export const Pwa = () => {
     return (
     <div>
         <img src={PWA} alt="Modo offline SmartPHone" className={styles.gifStyleSMT} />
-        <img src={PWAPC} alt="Modo offline PC" className={styles.gifStylepc} />
+        <img src={PWAPC} alt="Modo offline PC" className={styles.gifStylePC} />
     </div>
   )
 }
