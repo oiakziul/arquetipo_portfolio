5f35d19dbc7a0f1f0f4d921d03f05c4fc0882407
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-13 00:00:21 -0300

    fullscreenCurriculo44

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "9e42e296cd78763e8261e414a1e71e32"
   }, {
     "url": "index.html",
-    "revision": "0.7o7bnrv1b38"
+    "revision": "0.6qa7vrc9o78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
