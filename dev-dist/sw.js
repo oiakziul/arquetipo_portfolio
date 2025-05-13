2db7c7f89a1f52445e25db620809b301ab194663
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-13 18:20:51 -0300

    icon09

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "9e42e296cd78763e8261e414a1e71e32"
   }, {
     "url": "index.html",
-    "revision": "0.nbttdao3lg8"
+    "revision": "0.fersjalr6t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
