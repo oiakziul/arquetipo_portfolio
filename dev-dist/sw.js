fdfb6c559633b99f0307af9b2ebca7ac9315ef7d
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-13 19:11:56 -0300

    icon24

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "9e42e296cd78763e8261e414a1e71e32"
   }, {
     "url": "index.html",
-    "revision": "0.1q6sjdmpre8"
+    "revision": "0.4hb013uru3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
