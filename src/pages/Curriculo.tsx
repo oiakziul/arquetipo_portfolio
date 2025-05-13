a45872f252aba2d5e3fe2692f6fdc3a0c5b95ff9
Kaio Luiz Carmo de Brito <oiakziul@yahoo.com>
2025-05-12 21:03:16 -0300

    fullscreenCurriculo12

--- src/pages/Curriculo.tsx
+++ src/pages/Curriculo.tsx
@@ -1,10 +1,9 @@
-import curriculo from '../assets/images/CurriculoNew-1.png';
-import styles from'../assets/styles/components/curriculo.module.css';
+import curriculo from '../assets/images/CurriculoNew-1.jpg';
+import styles from '../assets/styles/components/curriculo.module.css';
 
 export const Curriculo = () => {
     return (
         <main className={styles.corpoCurriculo}>
-            <br />
             <div>
                 <img className={styles.imagemCurriculo} src={curriculo} alt="Curriculo" />
             </div>
